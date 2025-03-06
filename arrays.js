e83afcc669424b28937f53e0ee90a48c9072811d
pavani-byte <yuvapavani66@gmail.com>
2025-03-06 11:40:42 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,44 @@
+// task-1
+arr=[1,2,3,4,5]
+console.log(arr)
+
+// task-2
+arr=[1,2,3,4,5]
+console.log(arr(2))
+
+// task-3
+arr=[1,2,3,4,5]
+arr[1]=6
+console.log(arr)
+
+// task-4
+function Array(){
+    arr=[1,2,3,4,5]
+    return arr.length
+}
+console.log(Array())
+
+// task-5
+var new_Element=["hii",1,6,"hello",8]
+new_Element.push("Array")
+console.log(new_Element)
+
+// task-6
+var lastElement=["hii",1,6,"hello",8]
+lastElement.pop()
+console.log(lastElement)
+
+// task-7
+var eachelement=[1,2,3,4,5,6,7,8]
+console.log(element)
+for(var elements of eachelement )
+    console.log(eachelement)
+
+// task-9
+var arr1=arr
+console.log(arr1)
+
+// task-10
+var arr=['hello','everyone',]
+var result = arr.join(" ")
+console.log(result)
